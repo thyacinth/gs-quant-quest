436e3ae0fc009e0f57d8209f2ebbdb046d50cd0e
Xinhui Yong <xinhui.yong@nyu.edu>
2016-04-05 00:44:05 -0400

    Beginning to learn pywikibot

--- comparecompanies.py
+++ comparecompanies.py
@@ -1,4 +1,8 @@
 import numpy as np
 import pywikibot as pwb
 
-print "Hello World!"
+site = pwb.Site()
+page = pwb.Page(site, u"List of S&P 500 companies")
+item = pwb.ItemPage.fromPage(page)
+item.get()
+print item.toJSON()
