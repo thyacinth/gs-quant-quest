893d63bc6268936725f2d22a0a8336133b6bff23
Joseph Schiavone <jschiavone@palantir.com>
2016-04-05 12:24:46 -0400

    Added parser module and TODOs

--- comparecompanies.py
+++ comparecompanies.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def main(argv):
 	print "Hello World"
+	# TODO Xin: Call parsing methods here. Get us to the point where we have a data structure that contains all the companies and all the links.
 
 if __name__ == "__main__":
    main(sys.argv[1:])
---
+++ parser.py
@@ -0,0 +1,1 @@
+# TODO Xin - Add methods here that we will need. Try to think carefully about organization and how to separate functionality effectively.
